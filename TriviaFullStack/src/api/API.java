596fbbd3f68c8add8e17077440e3196b5be63d8c
midnightfire221 <sengthida.lorvan@gmail.com>
2024-02-27 21:12:03 -0500

    Edit classpath

--- TriviaFullStack/src/api/API.java
+++ TriviaFullStack/src/api/API.java
@@ -1,4 +1,3 @@
-package api;
 import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
--- TriviaFullStack/src/gui/Main.java
+++ TriviaFullStack/src/gui/Main.java
@@ -1,5 +1,3 @@
-package gui;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
