1039fb331a072217724ffdb60f26cfd75ffcdd41
midnightfire221 <sengthida.lorvan@gmail.com>
2024-02-27 20:41:36 -0500

    Add code

--- TriviaFullStack/src/gui/Main.java
+++ TriviaFullStack/src/gui/Main.java
@@ -1,5 +1,17 @@
 package gui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
